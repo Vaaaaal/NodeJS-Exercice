8af2ea1d0a3671b2605a28406a5f32e45011a89c
Vaaaaal <45296045+Vaaaaal@users.noreply.github.com>
2019-04-10 16:27:35 +0200

    Commit initial
    Premier commit

---
+++ app.js
@@ -0,0 +1,97 @@
+//jshint esversion:6
+
+// On requiere toutes les dependences dont on a besoin (se referer a leurs docs sur npm pour connaitre leurs fonctionnements etc)
+  // Express = serveur
+const express = require("express");
+  // Permet de faire transiter les datas
+const bodyParser = require("body-parser");
+  // Permet la création de templates
+const ejs = require("ejs");
+  // Permet de gerer la casse (pour l'URL dans ce cas-ci)
+const _ = require('lodash');
+
+
+  // Texte deja la de base dans le projet quand j'ai commencer, c'était pour l'avoir sur les différentes pages.
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+  // Création d'un tableau qui cotiendra les posts du blogs
+let posts = [];
+
+  // Configuration d'express pour utilisation
+const app = express();
+
+  // Configuration d'EJS pour pouvoir l'utiliser
+app.set('view engine', 'ejs');
+
+  // Configuration de body-parser pour utilisation
+app.use(bodyParser.urlencoded({extended: true}));
+  // Permet de donner le chemin d'acces pour le llien du style (qui se trouve dans header.ejs)
+app.use(express.static("public"));
+
+  // Route Home (page d'acceuil)
+app.get("/", function(req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts,
+  });
+});
+
+  // Route post détaillé
+app.get("/posts/:postName", function(req, res) {
+  const requestedTitle = req.params.postName;
+    // Passage dans chaque objet a l'interieur du tableau "posts"
+  posts.forEach(function(post) {
+      // Stocke le titre et le contenu dans une variable
+    const storedTitle = post.title;
+    const storedBody = post.body;
+
+        // Vérification si le contenu de l'indice (":postName") passer dans l'URL est le meme que le titre récupéré apres les avoir passer en kebab Case
+    if(_.kebabCase(requestedTitle) === _.kebabCase(storedTitle)) {
+        // Si c'est good on stocke le titre et le contenu du post dans une variable
+      res.render("post", {
+        title: storedTitle,
+        content: storedBody
+      })
+    }
+  })
+});
+
+  // Route About page
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
+
+  // Route Contact page
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
+
+  // Route Compose page (création d'article (post) pour le blog)
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+  // Route Compose page une fois qu'on a appuyer sur le bouton d'envoi
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  }
+    // Integre l'article a la fin du tableau
+  posts.push(post);
+
+    // Redirection une fois que le travail a été effectuer
+  res.redirect("/");
+});
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
